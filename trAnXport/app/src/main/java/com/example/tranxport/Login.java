dc6138fd12a3b61ff15d75ce6aeae758cddd351c
Arya <64455337+H-A-L-F@users.noreply.github.com>
2022-04-24 03:04:59 +0700

    Login Activity

--- trAnXport/app/src/main/java/com/example/tranxport/Login.java
+++ trAnXport/app/src/main/java/com/example/tranxport/Login.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private boolean isPassVis = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,12 +21,33 @@ public class Login extends AppCompatActivity {
 
         final EditText ETuname = findViewById(R.id.ET_username);
         final EditText ETpassword = findViewById(R.id.ET_password);
+        final ImageView passVis = findViewById(R.id.ic_passVis);
         final TextView signUpBtn = findViewById(R.id.btn_signup);
+
+        passVis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isPassVis) {
+                    isPassVis = true;
+
+                    ETpassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                    passVis.setImageResource(R.drawable.passVisible);
+                } else {
+                    isPassVis = false;
+
+                    ETpassword.setInputType(InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+                    passVis.setImageResource(R.drawable.passInvis);
+                }
+
+                //move cursor to last text
+                ETpassword.setSelection(ETpassword.length());
+            }
+        });
 
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent());
+                startActivity(new Intent(Login.this, Register.class));
             }
         });
     }
